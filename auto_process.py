3ab7168d73a9c13c221ea1876d0868e10c1e44f3
Junho Shin <junho.shin@nota.ai>
2023-09-05 12:09:02 +0900

    Export model to onnx format

--- auto_process.py
+++ auto_process.py
@@ -175,6 +175,113 @@ def train_run(opt):
     
     return opt
 
+def export_onnx(opt, save_path):
+    device = select_device(opt.export_device)
+    model = attempt_load(opt.weights, map_location=device)  # load FP32 model
+    labels = model.names
+
+    # Checks
+    gs = int(max(model.stride))  # grid size (max stride)
+    opt.img_size = [check_img_size(x, gs) for x in opt.img_size]  # verify img_size are gs-multiples
+
+    # Input
+    img = torch.zeros(1, 3, *opt.img_size).to(device)  # image size(1,3,320,192) iDetection
+
+    # Update model
+    for k, m in model.named_modules():
+        m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+        if isinstance(m, models.common.Conv):  # assign export-friendly activations
+            if isinstance(m.act, nn.Hardswish):
+                m.act = Hardswish()
+            elif isinstance(m.act, nn.SiLU):
+                m.act = SiLU()
+        # elif isinstance(m, models.yolo.Detect):
+        #     m.forward = m.forward_export  # assign forward (optional)
+    model.model[-1].export = not opt.grid  # set Detect() layer grid export
+    y = model(img)  # dry run
+    if opt.include_nms:
+        model.model[-1].include_nms = True
+        y = None
+
+    model.eval()
+    output_names = ['classes', 'boxes'] if y is None else ['output']
+    dynamic_axes = None
+    if opt.dynamic:
+        dynamic_axes = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
+            'output': {0: 'batch', 2: 'y', 3: 'x'}}
+    if opt.dynamic_batch:
+        opt.batch_size = 'batch'
+        dynamic_axes = {
+            'images': {
+                0: 'batch',
+            }, }
+        if opt.end2end and opt.max_wh is None:
+            output_axes = {
+                'num_dets': {0: 'batch'},
+                'det_boxes': {0: 'batch'},
+                'det_scores': {0: 'batch'},
+                'det_classes': {0: 'batch'},
+            }
+        else:
+            output_axes = {
+                'output': {0: 'batch'},
+            }
+        dynamic_axes.update(output_axes)
+    if opt.grid:
+        if opt.end2end:
+            print('\nStarting export end2end onnx model for %s...' % 'TensorRT' if opt.max_wh is None else 'onnxruntime')
+            model = End2End(model,opt.topk_all,opt.iou_thres,opt.conf_thres,opt.max_wh,device,len(labels))
+            if opt.end2end and opt.max_wh is None:
+                output_names = ['num_dets', 'det_boxes', 'det_scores', 'det_classes']
+                shapes = [opt.batch_size, 1, opt.batch_size, opt.topk_all, 4,
+                            opt.batch_size, opt.topk_all, opt.batch_size, opt.topk_all]
+            else:
+                output_names = ['output']
+        else:
+            model.model[-1].concat = True
+
+    torch.onnx.export(model, img, save_path, verbose=False, opset_version=12, input_names=['images'],
+                        output_names=output_names,
+                        dynamic_axes=dynamic_axes)
+
+    # Checks
+    onnx_model = onnx.load(save_path)  # load onnx model
+    onnx.checker.check_model(onnx_model)  # check onnx model
+
+    if opt.end2end and opt.max_wh is None:
+        for i in onnx_model.graph.output:
+            for j in i.type.tensor_type.shape.dim:
+                j.dim_param = str(shapes.pop(0))
+
+    # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+
+    # # Metadata
+    # d = {'stride': int(max(model.stride))}
+    # for k, v in d.items():
+    #     meta = onnx_model.metadata_props.add()
+    #     meta.key, meta.value = k, str(v)
+    # onnx.save(onnx_model, f)
+
+    if opt.simplify:
+        try:
+            import onnxsim
+
+            print('\nStarting to simplify ONNX...')
+            onnx_model, check = onnxsim.simplify(onnx_model)
+            assert check, 'assert check failed'
+        except Exception as e:
+            print(f'Simplifier failure: {e}')
+
+    # print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+    onnx.save(onnx_model, save_path)
+    print('ONNX export success, saved as %s' % save_path)
+
+    if opt.include_nms:
+        print('Registering NMS plugin for ONNX...')
+        mo = RegisterNMS(save_path)
+        mo.register_nms()
+        mo.save(save_path)
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -231,6 +338,21 @@ def parse_args():
     parser.add_argument('--artifact_alias', type=str, default="latest", help='version of dataset artifact to be used')
     parser.add_argument('--freeze', nargs='+', type=int, default=[0], help='Freeze layers: backbone of yolov7=50, first3=0 1 2')
     parser.add_argument('--v5-metric', action='store_true', help='assume maximum recall as 1.0 in AP calculation')
+
+    """
+        Export arguments
+    """
+    parser.add_argument('--dynamic', action='store_true', help='dynamic ONNX axes')
+    parser.add_argument('--dynamic-batch', action='store_true', help='dynamic batch onnx for tensorrt and onnx-runtime')
+    parser.add_argument('--grid', action='store_true', help='export Detect() layer grid')
+    parser.add_argument('--end2end', action='store_true', help='export end2end onnx')
+    parser.add_argument('--max-wh', type=int, default=None, help='None for tensorrt nms, int value for onnx-runtime nms')
+    parser.add_argument('--topk-all', type=int, default=100, help='topk objects for every images')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='iou threshold for NMS')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='conf threshold for NMS')
+    parser.add_argument('--simplify', action='store_true', help='simplify onnx model')
+    parser.add_argument('--include-nms', action='store_true', help='export end2end onnx')
+    parser.add_argument('--export-device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
 
     return parser.parse_args()
 
@@ -335,3 +457,19 @@ if __name__ == '__main__':
     opt = train_run(opt)
 
     logger.info("Fine-tuning step end.")
+
+    """
+        Export YOLOv5 model to onnx
+    """
+    logger.info("Export model to onnx format step start.")
+    
+    #model = reparam(opt)
+    #torch.save(model, COMPRESSED_MODEL_NAME + '_before_onnx.pt')
+    #opt.weights = COMPRESSED_MODEL_NAME + '_before_onnx.pt'
+
+    opt.weights = opt.save_dir + '/weights/best.pt'
+    export_onnx(opt, COMPRESSED_MODEL_NAME + '.onnx')
+    
+    logger.info(f'=> saving model to {COMPRESSED_MODEL_NAME}.onnx')
+
+    logger.info("Export model to onnx format step end.")
